b2171f74c430a5325449acf929eb6ba50ef2e7fc
Luca Colombini <luca.colombini81@gmail.com>
2023-08-12 16:59:15 +0200

    added user model and seed

---
+++ seeders/20230812143542-user.js
@@ -0,0 +1,32 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+    up: (queryInterface, Sequelize) => {
+        return queryInterface.bulkInsert('Users', [{
+            firstName: 'Luca',
+            lastName: 'Colombini',
+            email: 'lucacolombini81@gmail.com',
+            createdAt: new Date(),
+            updatedAt: new Date()
+        },
+        {
+            firstName: 'Homer',
+            lastName: 'Simpson',
+            email: 'hjsimpson@springfield.com',
+            createdAt: new Date(),
+            updatedAt: new Date()
+        },
+        {
+            firstName: 'Ned',
+            lastName: 'Flanders',
+            email: 'nedflanders@paradise.com',
+            createdAt: new Date(),
+            updatedAt: new Date()
+        }
+        ], {});
+    },
+    down: (queryInterface, Sequelize) => {
+        return queryInterface.bulkDelete('Users', null, {});
+    }
+};
