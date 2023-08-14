d14845298e90fc320a70d118d20e5ab2cb339a7d
Luca Colombini <luca.colombini81@gmail.com>
2023-08-14 09:08:28 +0200

    fix connection issue

--- database/database.provider.ts
+++ database/database.provider.ts
@@ -1,11 +1,5 @@
 const { Sequelize } = require('sequelize');
+const dotenv = require('dotenv')
+dotenv.config()
 
-export const sequelize = new Sequelize({
-    dialect: 'postgres',
-    host: process.env.DB_HOST,
-    port: 5432,
-    username: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
-});
-
+export const sequelize = new Sequelize(`postgres://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:5432/${process.env.DB_NAME}`);
